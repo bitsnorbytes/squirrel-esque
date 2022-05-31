bb4dbdddfbeda97b0b554c223ead9d29e52b0748
Punyasloka Parida <12557052+bitsnorbytes@users.noreply.github.com>
2022-05-31 09:03:54 +0530

    Small changes to design

--- web/src/components/TheHero.vue
+++ web/src/components/TheHero.vue
@@ -1,6 +1,7 @@
 <template>
- <div div class="flex">
-     <div class="flex justify-center w-3/4">
+<div class="m-h-screen bg-slate-800">
+ <div class="flex flex-wrap h-[5vh]">
+     <div class="flex justify-start w-3/4">
      <ul class="flex p-2 bg-red-600 text-3xl font-bold">
        <li class="p-2">X</li>
        <li class="p-2">Y</li>
@@ -15,20 +16,21 @@
   </div>
 
 
-  <nav class="w-1/4">
-   <ul class="flex justify-center py-2 px-3 bg-red-600 text-1xl font-bold">
-       <li class="py-2 px-6">HOME</li>
-       <li class="py-2 px-6">PROJECTS</li>
-       <li class="py-2 px-6">GITHUB</li>
+  <nav class="flex justify-end w-1/4">
+   <ul class="flex items-center py-2 px-3 bg-red-600 text-1xl font-bold">
+       <li class="py-2 px-6 tracking-widest">HOME</li>
+       <li class="py-2 px-6 tracking-widest">PROJECTS</li>
+       <li class="py-2 px-6 tracking-widest">GITHUB</li>
      </ul>
-     </nav>
-     
+     </nav> 
 
-    <div class="flex justify-center w-full">
+   
+</div>
+ <div class="flex items-center justify-center w-full h-[95vh]">
      <ul class="flex p-2 bg-red-600 text-3xl font-bold">
-       <li class="p-2">PROGRESS</li>
-       <li class="p-2">CODE</li>
-       <li class="p-2">Z</li>
+       <li class="p-2 tracking-widest">PROGRESS</li>
+       <li class="p-2 tracking-widest">CODE</li>
+       <li class="p-2 tracking-widest">Z</li>
      </ul>
      <ul class="flex ml-3 p-2 bg-red-600 text-3xl font-bold">
        <li class="p-2">1</li>
@@ -37,5 +39,5 @@
      </ul>  
 
   </div>
-</div>
+  </div>
 </template>
