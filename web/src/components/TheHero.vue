5f1500760fcf4265d57d964486441780eccbaa4c
Punyasloka Parida <12557052+bitsnorbytes@users.noreply.github.com>
2022-06-04 13:31:02 +0530

    Changes to Hero

--- web/src/components/TheHero.vue
+++ web/src/components/TheHero.vue
@@ -4,14 +4,15 @@
 
      <div class="flex justify-start w-full md:w-3/4 h-[10vh]">
      <ul class="flex items-center p-2 bg-red-600 text-3xl font-bold">
-       <li class="p-2">X</li>
-       <li class="p-2">Y</li>
-       <li class="p-2">Z</li>
+       <li class="px-3 md:px-4 py-2">P</li>
+       <li class="px-3 md:px-4 py-2">Y</li>
+       <li class="px-3 md:px-4 py-2">S</li>
+       <li class="px-3 md:px-4 py-2">L</li>
      </ul>
      <ul class="flex items-center ml-3 p-2 bg-red-600 text-3xl font-bold h-[10vh]">
        <li class="p-2">0</li>
-       <li class="p-2">0</li>
-       <li class="p-2">0</li>
+       <li class="p-2">2</li>
+       <li class="p-2">%</li>
      </ul>  
 
   </div>
@@ -26,17 +27,28 @@
      </nav> 
    
 </div>
- <div class="flex flex-wrap md:flex-nowrap items-center justify-center w-full h-screen">
-     <ul class="flex p-2 bg-red-600 text-3xl font-bold">
-       <li class="p-2 tracking-widest">PROGRESS</li>
-       <li class="p-2 tracking-widest">CODE</li>
-       <li class="p-2 tracking-widest">Z</li>
+<div class="flex flex-wrap items-center justify-center w-full h-[40vh]"></div>
+ <div class="flex flex-wrap content-center w-full h-[50vh] bg-red-600">
+     <ul class="flex p-2 relative items-center text-6xl w-full h-[5rem] font-bold">
+       <li class="p-2 flex items-center justify-end tracking-widest text-right h-full w-10/12 md:w-1/2">DEVOPS</li>
+       <li class="h-full w-2/12 flex items-center justify-start"><svg xmlns="http://www.w3.org/2000/svg" width="83" height="100%" viewBox="0 0 83 97">
+  <g id="Polygon_1" data-name="Polygon 1" transform="translate(83) rotate(90)" fill="#850081">
+    <path d="M 88.28727722167969 78 L 8.712726593017578 78 L 48.5 9.910442352294922 L 88.28727722167969 78 Z" stroke="none"/>
+    <path d="M 48.5 19.8209342956543 L 17.42548370361328 73 L 79.57451629638672 73 L 48.5 19.8209342956543 M 48.5 7.62939453125e-06 L 97 83 L 0 83 L 48.5 7.62939453125e-06 Z" stroke="none" fill="#230101"/>
+  </g>
+</svg>
+</li>
      </ul>
-     <ul class="flex ml-3 p-2 bg-red-600 text-3xl font-bold">
-       <li class="p-2">0</li>
-       <li class="p-2">2</li>
-       <li class="py-2 px-1">%</li>
-     </ul>  
+     <ul class="flex p-2 relative items-center text-6xl w-full h-[5rem] font-bold">
+       <li class="p-2 flex items-center justify-end tracking-widest text-right h-full w-10/12 md:w-1/2">ART</li>
+       <li class="h-full w-2/12 flex items-center justify-start"><svg xmlns="http://www.w3.org/2000/svg" width="83" height="100%" viewBox="0 0 83 97">
+  <g id="Polygon_2" data-name="Polygon 2" transform="translate(0 97) rotate(-90)" fill="#850081">
+    <path d="M 88.28727722167969 78 L 8.712726593017578 78 L 48.5 9.910438537597656 L 88.28727722167969 78 Z" stroke="none"/>
+    <path d="M 48.5 19.82093048095703 L 17.42548370361328 73 L 79.57451629638672 73 L 48.5 19.82093048095703 M 48.5 0 L 97 83 L 0 83 L 48.5 0 Z" stroke="none" fill="#230101"/>
+  </g>
+</svg>
+</li>
+     </ul>
 
   </div>
   </div>
